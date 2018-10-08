56772f3c70a0130a06ddba2b7b7487fac0b9d239
kawashima-mikiya <kawashimamikiya@kawashimatadashikinarinoMacBook-puro.local>
2018-10-08 14:10:37 +0900

    Group_fanction

--- chat-space/app/controllers/users_controller.rb
+++ chat-space/app/controllers/users_controller.rb
@@ -11,15 +11,7 @@ class UsersController < ApplicationController
     end
   end
 
-  def create
-	@group = Group.new(group_params)
-	if @group.save
-	  redirect_to root_path, notice: "グループを作成しました"
-	else
-	  flash.now[:alert] = "グループ名を入力してください"
-	  render :new
-	end
-  end
+
 
   private
 
